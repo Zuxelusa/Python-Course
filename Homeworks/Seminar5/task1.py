1060325a6ecc2fbde07290552bf8b5200894317a
Aleksandr Solovev <zuxelusa@ya.ru>
2022-10-23 22:19:11 +0300

    task 1 ready

---
+++ Homeworks/Seminar5/task1.py
@@ -0,0 +1,9 @@
+'''Напишите программу, удаляющую из текста все слова, содержащие ""абв"".'''
+
+def my_replace(text: str, excl: str):
+    lst = [i for i in text.split() if excl not in i]
+    return " ".join(lst)
+
+text = "Привет, моя абвгдейка, привет абв! и ты туАБВда"
+excl = "абв"
+print(my_replace(text, excl))
